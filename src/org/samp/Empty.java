6487809b5c6a7bba2dfa09ece8f4c1b4e72040ec
PriyankaMullai <nanthinimppm@gmail.com>
2021-05-05 23:54:41 -0700

    Task Completed

---
+++ src/org/samp/Empty.java
@@ -0,0 +1,23 @@
+package org.samp;
+
+public class Empty {
+	
+	private void add() {
+System.out.println("ADD");
+	}
+	
+	
+public static void main(String[] args) {
+	
+	
+	Empty e= new Empty();
+	e.add();
+	
+	
+	
+	
+	
+	
+	
+}
+}
